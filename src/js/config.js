907d11bdc8a1b95e117dde70db320e80d0d1896a
Morten Gulden <morteng@gmail.com>
2023-02-09 13:03:45 +0100

    minipaint fixes

--- src/js/config.js
+++ src/js/config.js
@@ -2,19 +2,19 @@
 
 var config = {};
 
-config.TRANSPARENCY = false;
-config.TRANSPARENCY_TYPE = 'squares'; //squares, green, grey
+config.TRANSPARENCY = true;
+config.TRANSPARENCY_TYPE = 'grey'; //squares, green, grey
 config.LANG = 'en';
 config.WIDTH = null;
 config.HEIGHT = null;
 config.visible_width = null;
 config.visible_height = null;
-config.COLOR = '#008000';
+config.COLOR = '#ffffff';
 config.ALPHA = 255;
 config.ZOOM = 1;
-config.SNAP = true;
+config.SNAP = false;
 config.pixabay_key = '3ca2cd8af3fde33af218bea02-9021417';
-config.safe_search_can_be_disabled = true;
+config.safe_search_can_be_disabled = false;
 config.google_webfonts_key = 'AIzaSyAC_Tx8RKkvN235fXCUyi_5XhSaRCzNhMg';
 config.layers = [];
 config.layer = null;
